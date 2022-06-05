e0e272cc8bed4693510c13495c52acb212601fdf
robel <robelserekebrhan@gmail.com>
2022-06-04 22:21:41 -0600

    i just initialized git

---
+++ store_management.py
@@ -0,0 +1,67 @@
+
+import csv
+class items:
+         pay_rate = 0.8  # class attribute
+         profit_margin = 3.2 # class attribute
+         All = []
+         
+    # is called special funtion or magic methood and is used to initialize all the attributes of the innstances(objects)    
+         def __init__(self, item_code, item_name, description, buying_price, selling_price=0): 
+        
+        
+                # attributes for every created instance(object)
+                self.item_code = item_code
+                self.item_name =  item_name
+                #self.phoeto = phoeto
+                self.description = description
+                self.buying_price = buying_price
+                self.selling_price = buying_price * items.profit_margin 
+
+
+        # validate the data inputed  
+                assert buying_price >=0, f"buying_price{buying_price} can not be negative"
+
+        # to see all the attribute that bellong to the class or object using the magic attribute(not majic method) .__dict__
+        #print(items.__dict__)
+        #print(item1.__dict__)
+                
+         
+        # add our instances to the empty list we name it All
+                items.All.append(self)
+
+
+        
+        # an instance method to appy a discount from he payrate given as attribute of instance first if not found from attribute of class
+         def apply_discount(self):
+                 self.selling_price = self.selling_price * self.pay_rate
+
+        
+        
+        # automate instantiating 
+                                                                                            
+         @classmethod
+         def ins_csv(cls):
+                 with open('/home/robel/oop/storemanagement/Untitled.csv', 'r') as f:
+                         reader = csv.DictReader(f)
+                         items_list = list(reader)
+
+
+                         for item in items_list:
+                                  items(
+                                          item_code=item.get('item_code: str'),
+                                          item_name=item.get('item_name: str'),
+                                          description=item.get('description: str'),
+                                          buying_price=item.get('buying_price: float')
+                                          )
+                                
+
+
+
+        #lets use the repr magic method to format all our instances in the All list in the way we insanciate our instances
+
+         def __repr__(self):
+                 return f"items('{self.item_code}', '{self.item_name}','{self.description}', {self.buying_price} )"
+
+
+items().ins_csv()
+print(items.All)
